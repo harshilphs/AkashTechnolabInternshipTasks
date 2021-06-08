8b264e07654ad572da0589e44e5a5e8c46ed3fbb
Harshil Padhiyar <66584783+harshilphs@users.noreply.github.com>
2021-06-08 06:31:32 +0530

    Update settings.py

--- EmployerManagementSystem/EmployerManagementSystem/settings.py
+++ EmployerManagementSystem/EmployerManagementSystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '90-_)%tgoguu(m#r7=7n1f9%wyh%6b$)j%e9vf87_mb-2d8c+r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
